a4d61a5110276b56eb8f6f58fcd8a64168dbf3b4
daniel <danielpn23@outlook.com>
2022-10-16 11:16:06 -0300

    Exception handling for UserService method deleteUser

--- src/main/java/com/project/store/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/project/store/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.project.store.resources.exceptions;
 
+import com.project.store.services.exceptions.DatabaseException;
 import com.project.store.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,5 +24,21 @@ public class ResourceExceptionHandler {
         );
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    // ExceptionHandler para tratar a exceção causada pela DataIntegrityViolationException
+    // Essa exceção é lançada quando existe uma tentativa de violar a integridade do banco de dados
+    // Exemplo: deletar um usuário que possui pedidos associados à conta
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request) {
+        StandardError error = new StandardError(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Database error",
+                e.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/com/project/store/services/UserService.java
+++ src/main/java/com/project/store/services/UserService.java
@@ -2,8 +2,11 @@ package com.project.store.services;
 
 import com.project.store.entities.User;
 import com.project.store.repositories.UserRepository;
+import com.project.store.services.exceptions.DatabaseException;
 import com.project.store.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void deleteUser(Long id) {
-        userRepository.deleteById(id);
+        try{
+            userRepository.deleteById(id);
+        } catch(EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch(DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User updateUser(Long id, User obj) {
